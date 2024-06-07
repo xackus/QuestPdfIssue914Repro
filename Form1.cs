67b4ac8779395f468e42fe4235b2880d5622a020
Maciej Walczak <14938807+xackus@users.noreply.github.com>
2024-06-07 11:10:22 +0200

    Add repro

---
+++ Form1.cs
@@ -0,0 +1,19 @@
+﻿using QuestPDF.Infrastructure;
+using System;
+using System.Windows.Forms;
+
+namespace QuestPdfIssue914Repro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            QuestPDF.Settings.License = LicenseType.Professional;
+        }
+    }
+}
